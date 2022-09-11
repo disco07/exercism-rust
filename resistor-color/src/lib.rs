bf6583da7044e79509aa48dbf95d092d7310320c
disco07 <koneenok@outlook.fr>
2022-09-11 19:18:03 +0200

    resistor color

--- resistor-color/src/lib.rs
+++ resistor-color/src/lib.rs
@@ -1,3 +1,7 @@
+extern crate core;
+
+use crate::ResistorColor::*;
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum ResistorColor {
     Black,
@@ -17,12 +21,32 @@ pub fn color_to_value(_color: ResistorColor) -> u32 {
 }
 
 pub fn value_to_color_string(value: u32) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    let color = colors().get((value as usize));
+    match color {
+        Some(x) => x.to_string(),
+        None => panic!("Index not found"),
+    }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    let mut vec: Vec<ResistorColor> = vec![Black, Brown, Red, Orange, Yellow, Green, Blue, Violet, Grey, White];
+    vec
+}
+
+impl ToString for ResistorColor {
+    fn to_string(&self) -> String {
+        match self {
+            Black => "Black".to_string(),
+            Brown => "Brown".to_string(),
+            Red => "Red".to_string(),
+            Orange => "Orange".to_string(),
+            Yellow => "Yellow".to_string(),
+            Green => "Green".to_string(),
+            Blue => "Blue".to_string(),
+            Violet => "Violet".to_string(),
+            Grey => "Grey".to_string(),
+            White => "White".to_string(),
+            _ => panic!("enum not find"),
+        }
+    }
 }
