91b9e97bb07dbeb5c1cae8c1afefc76e74976501
disco07 <koneenok@outlook.fr>
2022-10-08 21:30:42 +0200

    lowpower

--- low-power-embedded-game/src/lib.rs
+++ low-power-embedded-game/src/lib.rs
@@ -13,12 +13,6 @@ pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
 pub struct Position(pub i16, pub i16);
 impl Position {
     pub fn manhattan(&mut self) -> i16 {
-        if self.0 < 0 {
-            self.0 *= -1;
-        }
-        if self.1 < 0 {
-            self.1 *= -1;
-        }
-        self.0 + self.1
+        self.0.abs() + self.1.abs()
     }
 }
