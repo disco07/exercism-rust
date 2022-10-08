757e830e06cd4f97d49a54589944c51c2598535c
disco07 <koneenok@outlook.fr>
2022-10-08 21:29:50 +0200

    lowpower

--- low-power-embedded-game/src/lib.rs
+++ low-power-embedded-game/src/lib.rs
@@ -7,16 +7,18 @@ pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    if iter[0]%2 == 0 {
-        iter.filter(|&x| *x % 2 == 0)
-    } else {
-        iter
-    }
+    iter.step_by(2)
 }
 
 pub struct Position(pub i16, pub i16);
 impl Position {
-    pub fn manhattan(&self) -> i16 {
-        unimplemented!("implement `fn manhattan`")
+    pub fn manhattan(&mut self) -> i16 {
+        if self.0 < 0 {
+            self.0 *= -1;
+        }
+        if self.1 < 0 {
+            self.1 *= -1;
+        }
+        self.0 + self.1
     }
 }
