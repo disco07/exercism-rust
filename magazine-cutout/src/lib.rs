b6064f2aca225c4b312b1fbcddd9284f9ae50f33
disco07 <koneenok@outlook.fr>
2023-02-07 16:18:31 +0100

    fibonacci

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -5,5 +5,25 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut notes: HashMap<&str, usize> = HashMap::new();
+
+    for n in note {
+        *notes.entry(n).or_insert(0) += 1;
+    }
+
+    for mag in magazine {
+        let nb_word = notes.get(mag).unwrap_or(&0);
+        let new_nb_word = || {
+            match nb_word {
+                0 => 0,
+                _ => nb_word - 1
+            }
+        };
+
+        notes.insert(mag, new_nb_word());
+    }
+
+    let val: Vec<_> = notes.values().collect();
+    println!("{:?}", val.iter().max());
+    *val.iter().max().unwrap() == &0_usize
 }
