79512bcdc98078fba5e8d435c9c3e8a71504f691
disco07 <koneenok@outlook.fr>
2023-02-08 12:31:32 +0100

    clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,11 +1,27 @@
-pub struct Clock;
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Clock {
+    minutes: i32,
+}
+
+const DAY: i32 = 24 * 60;
+const HOUR: i32 = 60;
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:02}:{:02}", self.minutes / HOUR, self.minutes%HOUR)
+    }
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        Self {
+            minutes: (((hours * HOUR + minutes) % DAY) + DAY) % DAY,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {minutes} minutes to existing Clock time");
+        Clock::new(0, self.minutes + minutes)
     }
 }
