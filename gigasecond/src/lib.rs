3e79492c72135c8bb510b9bf4ea0c562b354959a
disco07 <koneenok@outlook.fr>
2023-02-08 09:35:05 +0100

    gigasecond

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,7 @@
+use time::ext::NumericalDuration;
 use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {start}");
+    start + 1_000_000_000.seconds()
 }
